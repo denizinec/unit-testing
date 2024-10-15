9ba2cc08ba75a5088098497b91dc0533fb594f2a
Deniz İneç <denizinec9@gmail.com>
2024-10-15 06:26:37 +0300

    initial commit

---
+++ src/main/java/Business/SomeBusinessImpl.java
@@ -0,0 +1,12 @@
+package Business;
+
+public class SomeBusinessImpl {
+
+    public int calculateSum(int[] data){
+        int sum = 0;
+        for(int value:data){
+            sum += value;
+        }
+        return sum;
+    }
+}
---
+++ src/test/java/SomeBusinessImplTest.java
@@ -0,0 +1,28 @@
+import Business.SomeBusinessImpl;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SomeBusinessImplTest {
+
+    @Test
+    void calculateSum_basic() {
+        SomeBusinessImpl business = new SomeBusinessImpl();
+        int actualResult = business.calculateSum(new int[] {1,2,3});
+        int expectedResult = 6;
+        assertEquals(expectedResult,actualResult);
+
+
+    }
+
+    @Test
+    void calculateSum_empty() {
+        SomeBusinessImpl business = new SomeBusinessImpl();
+        int actualResult = business.calculateSum(new int[] {});
+        int expectedResult = 0;
+        assertEquals(expectedResult,actualResult);
+
+
+    }
+
+}
